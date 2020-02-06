8a77a5ac5ae3eac411dcac1ca81b309670931f84
Eric Wilson <ewpiano@gmail.com>
2020-02-06 11:56:43 -0600

    Added switch to toggle UI from dark to light. Also added functionality to close menu by clicking anywhere on menu except menu item.

--- app.js
+++ app.js
@@ -18,8 +18,25 @@ $('#menu-close').click(function() {
   $('#menu-window').addClass('hidden');
 });
 
+// Close menu if menu window is clicked, but not menu item
+$('#menu-window').click(function(e) {
+  if($(e.target).children()[0] != undefined) {
+    $('#menu-window').addClass('hidden');
+  }
+});
+
+// Dark <--> Light UI
+$('#ui-trigger').click(function() {
+  let n = $('#ui:checked').length;
+  if (n === 0) {
+    $('body').addClass('light');
+  } else if (n === 1) {
+    $('body').removeClass('light');
+  }
+});
+
 // Change style of Pads when clicked.
-$('.pad').click(function(event) {
+$('.pad').mousedown(function(event) {
   $(this).children().addClass('pressed');
   setTimeout(function() {
     $('.pad-inner').removeClass('pressed');
@@ -29,8 +46,16 @@ $('.pad').click(function(event) {
 // Pad Sounds Setup
 // pad = #id
 // sound = filepath to sound
+// shortcut = ASCII Code (integer)
 function registerSound(pad, sound, shortcut) {
-  $(pad).click(function() {
+  $(document).keydown(function(event) {
+    let keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == shortcut) {
+      $(pad).mousedown();
+    }
+  });
+
+  $(pad).mousedown(function() {
     let audio = new Audio(sound);
     audio.currentTime = 0;
     audio.play();
@@ -38,29 +63,29 @@ function registerSound(pad, sound, shortcut) {
 }
 // Row 1
 // Cymbals
-registerSound('#pad1', 'audio/cymbals/cymbal-crash.mp3');
-registerSound('#pad2', 'audio/cymbals/cymbal-crash-hard.mp3');
-registerSound('#pad3', 'audio/cymbals/cymbal-ride-2.mp3');
-registerSound('#pad4', 'audio/cymbals/cymbal-ride.mp3');
+registerSound('#pad1', 'audio/cymbals/cymbal-crash.mp3', 81);
+registerSound('#pad2', 'audio/cymbals/cymbal-crash-hard.mp3', 87);
+registerSound('#pad3', 'audio/cymbals/cymbal-ride-2.mp3', 69);
+registerSound('#pad4', 'audio/cymbals/cymbal-ride.mp3', 82);
 
 // Row 2
 // Hi-Hat
 // Snares
-registerSound('#pad5', 'audio/hi-hat/hi-hat.mp3');
-registerSound('#pad6', 'audio/hi-hat/hi-hat-2.mp3');
-registerSound('#pad7', 'audio/cymbals/hi-hat/hi-hat-3.mp3');
-registerSound('#pad8', 'audio/snares/snare.mp3');
+registerSound('#pad5', 'audio/hi-hat/hi-hat.mp3', 65);
+registerSound('#pad6', 'audio/hi-hat/hi-hat-3.mp3', 83);
+registerSound('#pad7', 'audio/hi-hat/hi-hat-4.mp3', 68);
+registerSound('#pad8', 'audio/snares/snare.mp3', 70);
 
 // Row 3
 // Snares & Toms
-registerSound('#pad9', 'audio/snares/snare-80s.mp3');
-registerSound('#pad10', 'audio/snares/rimshot.mp3');
-registerSound('#pad11', 'audio/toms/tom.mp3');
-registerSound('#pad12', 'audio/toms/tom-2.mp3');
+registerSound('#pad9', 'audio/snares/snare-80s.mp3', 85);
+registerSound('#pad10', 'audio/snares/rimshot.mp3', 73);
+registerSound('#pad11', 'audio/toms/tom.mp3', 79);
+registerSound('#pad12', 'audio/toms/tom-2.mp3', 80);
 
 // Row 4
 // Toms & Kick
-registerSound('#pad13', 'audio/toms/tom-3.mp3');
-registerSound('#pad14', 'audio/kick/kick.mp3');
-registerSound('#pad15', 'audio/kick/kick-2.mp3');
-registerSound('#pad16', 'audio/kick/kick-3.mp3');
+registerSound('#pad13', 'audio/toms/tom-3.mp3', 72);
+registerSound('#pad14', 'audio/kick/kick.mp3', 74);
+registerSound('#pad15', 'audio/kick/kick-2.mp3', 75);
+registerSound('#pad16', 'audio/kick/kick-3.mp3', 76);
